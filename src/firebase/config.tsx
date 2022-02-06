a9d105db4b68324221c7d2dd2769fabbdeb87d34
takuma <takuma.watanabe@meetsmore.com>
2022-02-06 22:38:53 +0900

    rename product name

--- src/firebase/config.tsx
+++ src/firebase/config.tsx
@@ -1,18 +1,18 @@
-import React from "react"
-import firebase from "firebase/app"
-import "firebase/storage"
-import "firebase/firestore"
-import "firebase/auth"
+import firebase from 'firebase/app'
+import 'firebase/storage'
+import 'firebase/firestore'
+import 'firebase/auth'
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyAkWMnIFZhdea7obegh6ked-Kcl9Yoz0pw",
-  authDomain: "sherenote-0920.firebaseapp.com",
-  databaseURL: "https://sherenote-0920-default-rtdb.firebaseio.com",
-  projectId: "sherenote-0920",
-  storageBucket: "sherenote-0920.appspot.com",
-  messagingSenderId: "366733885061",
-  appId: "1:366733885061:web:abd1767f95da2514a0f3ec"
+  apiKey: 'AIzaSyCsEcJGVCXkAS8WbGJznB71iY7r_Ca8B20',
+  authDomain: 'share-note-0920.firebaseapp.com',
+  databaseURL: 'https://share-note-0920-default-rtdb.firebaseio.com',
+  projectId: 'share-note-0920',
+  storageBucket: 'share-note-0920.appspot.com',
+  messagingSenderId:
+    'AAAA4PYc0_A:APA91bGGxWGn-W6GpYeci1rLFH-Gc_ugapom60DmWw12sOUpkIMKDBjyxyh_dytwGzHySSrUpgMpXA-uCENdJcRYJqrx3H2JMOwfOxmF2G3gzw0jw91mFrh9zxm0zHQRphl_1JdG00YN',
+  // appId: '1:366733885061:web:abd1767f95da2514a0f3ec',
 }
 
 // Initialize Firebase
@@ -21,7 +21,5 @@ if (firebase.apps.length === 0) {
 }
 
 const storage = firebase.storage()
-
-// const storage = {}
 
 export { firebase, storage }
