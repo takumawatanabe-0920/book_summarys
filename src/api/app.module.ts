9a09c6c38b756eac9ea0e5722598b038dfaf6327
takuma <takuma.watanabe@meetsmore.com>
2022-07-17 23:34:02 +0900

    add mongoose 導入

--- src/api/app.module.ts
+++ src/api/app.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
-
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import * as config from 'config';
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      config.get('mongodb.uri'),
+      config.get('mongodb.options'),
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
