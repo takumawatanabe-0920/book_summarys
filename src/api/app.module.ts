e4dc366ec86aa1596b34ec734d8dc01ebb8e51ef
takuma <takuma.watanabe@meetsmore.com>
2022-07-18 10:44:55 +0900

    add flame

--- src/api/app.module.ts
+++ src/api/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoryController } from './category/core/category.controller';
+import { CategoryService } from './category/core/category.service';
+import { CategoryModule } from './category/core/category.module';
 import * as config from 'config';
 @Module({
   imports: [
@@ -9,8 +12,9 @@ import * as config from 'config';
       config.get('mongodb.uri'),
       config.get('mongodb.options'),
     ),
+    CategoryModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, CategoryController],
+  providers: [AppService, CategoryService],
 })
 export class AppModule {}
