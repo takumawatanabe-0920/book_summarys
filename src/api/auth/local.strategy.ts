09dd0032bd6b0b6e23290a729e5249afa1a687fa
takuma <takuma.watanabe@meetsmore.com>
2022-07-28 00:37:45 +0900

    add type fix

--- src/api/auth/local.strategy.ts
+++ src/api/auth/local.strategy.ts
@@ -1,10 +1,14 @@
 import { Strategy } from 'passport-local';
-import { PassportStrategy } from '@nestjs/passport';
+import { PassportStrategy, AbstractStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthApplication } from './auth.application';
 
+const PassportJwtStrategy = PassportStrategy(Strategy) as new (
+  ...args
+) => AbstractStrategy & Strategy;
+
 @Injectable()
-export class LocalStrategy extends PassportStrategy(Strategy) {
+export class LocalStrategy extends PassportJwtStrategy {
   constructor(private authApplication: AuthApplication) {
     super();
   }
