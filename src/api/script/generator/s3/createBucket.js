a59fa3f7ff3e60387d8d92355fafee277d04a112
takuma <takuma.watanabe@meetsmore.com>
2022-08-14 00:06:20 +0900

    add commandでS3の操作をする

--- src/api/script/generator/s3/createBucket.js
+++ src/api/script/generator/s3/createBucket.js
@@ -1,29 +1,29 @@
-// const config = require('config');
-// // Load the AWS SDK for Node.js
-// var AWS = require('aws-sdk');
-// // Set the region
-// AWS.config.update({
-//   region: config.get('aws.region'),
-//   credentials: {
-//     accessKeyId: config.get('aws.accessKeyId'),
-//     secretAccessKey: config.get('aws.secretAccessKey'),
-//   },
-// });
+const config = require('config');
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region
+AWS.config.update({
+  region: config.get('aws.region'),
+  credentials: {
+    accessKeyId: config.get('aws.accessKeyId'),
+    secretAccessKey: config.get('aws.secretAccessKey'),
+  },
+});
 
-// // Create S3 service object
-// var s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+// Create S3 service object
+var s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 
-// // Create the parameters for calling createBucket
-// var bucketParams = {
-//   Bucket: process.argv[2],
-// };
+// Create the parameters for calling createBucket
+var bucketParams = {
+  Bucket: process.argv[2],
+};
 
-// // call S3 to create the bucket
-// s3.createBucket(bucketParams, function (err, data) {
-//   console.log({ err, data });
-//   if (err) {
-//     console.log('Error', err);
-//   } else {
-//     console.log('Success', data.Location);
-//   }
-// });
+// call S3 to create the bucket
+s3.createBucket(bucketParams, function (err, data) {
+  console.log({ err, data });
+  if (err) {
+    console.log('Error', err);
+  } else {
+    console.log('Success', data.Location);
+  }
+});
