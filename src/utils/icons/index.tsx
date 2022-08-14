e7ef12c0b6ae2c53c093f1271179962de27f81a7
takuma <takuma.watanabe@meetsmore.com>
2022-08-14 17:00:15 +0900

    add logo追加 README修正

--- src/utils/icons/index.tsx
+++ src/utils/icons/index.tsx
@@ -3,6 +3,6 @@ import iconOl from '../../static/images/icon/list-ol-solid.svg';
 import editIcon from '../../static/images/icon/edit-regular.svg';
 import caretDownIcon from '../../static/images/icon/caret-down-solid.svg';
 import userCircleIcon from '../../static/images/icon/user-circle-solid.svg';
-import logoIcon from '../../static/images/icon/logo.png';
+import logoIcon from '../../static/images/icon/logo3.png';
 
 export { iconUl, iconOl, editIcon, userCircleIcon, caretDownIcon, logoIcon };
